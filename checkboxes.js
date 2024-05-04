ee7a1ae826db5dd2221423df7b603210d2254f92
christoph1j2 <ernstleska@gmail.com>
2024-05-04 18:43:41 +0200

    fixed color changing (brute force)

--- checkboxes.js
+++ checkboxes.js
@@ -1,25 +1,39 @@
 function checklist(checklistContainerId,processContainerId,stationContainerId) {
   const checkboxes = Array.from(document.getElementById(checklistContainerId).querySelectorAll('input[type="checkbox"]'));
 
-  function getStationColor(allCheckboxes) {
-    const allChecked = allCheckboxes.every(checkbox => checkbox.checked && checkbox.disabled !== true);
-    const allUnchecked = allCheckboxes.every(checkbox => !checkbox.checked);
-    
-    if (allUnchecked) {
-      console.log("red");
-      return "red"; // All checkboxes unchecked
-    } else if (allChecked) {
-      console.log("green");
-      return "green"; // All enabled checkboxes are checked
-    } else {
-      console.log("orange");
-      return "orange"; // Some checkboxes checked, but not all or some are disabled
+  function getStationColor(stationContainerId) {
+    let c1,c2,c3,c4;
+    if (stationContainerId == 's1'){
+      c1 = document.getElementById("d1p1"); c2 = document.getElementById("d1p2"); c3 = document.getElementById("d1p3"); c4 = document.getElementById("d1p4");
+      if(c1.checked && c2.checked && c3.checked && c4.checked) { return 'green' }
+      else if(c1.checked || c2.checked || c3.checked || c4.checked) { return 'orange' }
+      else if(!(c1.checked && c2.checked && c3.checked && c4.checked)) { return 'red' }
     }
-
+    if (stationContainerId == 's2'){
+      c1 = document.getElementById("d2p1"); c2 = document.getElementById("d2p2"); c3 = document.getElementById("d2p3"); c4 = document.getElementById("d2p4");
+      if(c1.checked && c2.checked && c3.checked && c4.checked) { return 'green' }
+      else if(c1.checked || c2.checked || c3.checked || c4.checked) { return 'orange' }
+      else if(!(c1.checked && c2.checked && c3.checked && c4.checked)) { return 'red' }
+    }
+    if (stationContainerId == 's3'){
+      c1 = document.getElementById("d3p1"); c2 = document.getElementById("d3p2"); c3 = document.getElementById("d3p3"); c4 = document.getElementById("d3p4");
+      if(c1.checked && c2.checked && c3.checked && c4.checked) { return 'green' }
+      else if(c1.checked || c2.checked || c3.checked || c4.checked) { return 'orange' }
+      else if(!(c1.checked && c2.checked && c3.checked && c4.checked)) { return 'red' }
+    }
+    if (stationContainerId == 's4'){
+      c1 = document.getElementById("d4p1"); c2 = document.getElementById("d4p2"); c3 = document.getElementById("d4p3"); c4 = document.getElementById("d4p4");
+      if(c1.checked && c2.checked && c3.checked && c4.checked) { return 'green' }
+      else if(c1.checked || c2.checked || c3.checked || c4.checked) { return 'orange' }
+      else if(!(c1.checked && c2.checked && c3.checked && c4.checked)) { return 'red' }
+    }
+    if (stationContainerId == 's5'){
+      c1 = document.getElementById("d5p1"); c2 = document.getElementById("d5p2"); c3 = document.getElementById("d5p3"); c4 = document.getElementById("d5p4");
+      if(c1.checked && c2.checked && c3.checked && c4.checked) { return 'green' }
+      else if(c1.checked || c2.checked || c3.checked || c4.checked) { return 'orange' }
+      else if(!(c1.checked && c2.checked && c3.checked && c4.checked)) { return 'red' }
+    }
   }
-
-
-  
   let previous = false;
   checkboxes.forEach((checkbox, index) => {
     checkbox.addEventListener("click", () => {
@@ -38,8 +52,7 @@ function checklist(checklistContainerId,processContainerId,stationContainerId) {
 
       // Station Color
       const station = document.getElementById(stationContainerId)
-      const allCheckboxes = stationContainer.querySelectorAll('input[type="checkbox"]');
-      const stationColor = getStationColor(allCheckboxes);
+      const stationColor = getStationColor(stationContainerId);
       station.style.backgroundColor = stationColor;
         
     });
