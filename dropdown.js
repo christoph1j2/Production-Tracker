e542e7cac460674d53b6278a417c7d40ce2ec2c7
christoph1j2 <ernstleska@gmail.com>
2024-05-05 13:21:03 +0200

    added the rest of html structure, new css rules

--- dropdown.js
+++ dropdown.js
@@ -1,6 +1,8 @@
 function dropdown(checklistContainerId){
     const header = document.querySelector(`#${checklistContainerId} #hdr`);
     const container = document.querySelector(`#${checklistContainerId} #content`);
+    const arrow = document.querySelector(`#${checklistContainerId} #hdr i`)
+
     let count = 0;
 
     header.addEventListener("click", () => {
@@ -8,12 +10,14 @@ function dropdown(checklistContainerId){
         switch (count%2) {
             case 0:
                 container.style.display = 'none';
+                arrow.innerHTML = '&darr;';
                 break;
             case 1:
                 container.style.display = 'block';
+                arrow.innerHTML = '&uarr;';
                 break;
             default:
-                console.error("jsi pica")
+                console.error("du bist deppert")
                 break;
         }
     })
